b42c7313a1cf3f4afd9f7f5dd303539844dec105
jrpndev <jrpn.env@gmail.com>
2024-11-21 18:27:38 -0400

    feat : adicionando nginx

--- views/project/src/core/api/index.tsx
+++ views/project/src/core/api/index.tsx
@@ -1,4 +1,4 @@
 const environment = process.env.ENVIRONMENT;
-let BASE_URL = environment == 'PRODUCTION' ? "http:31783419.com" : "http://localhost:5000"
+let BASE_URL = environment === 'PRODUCTION' ? "http://18.228.205.132:5000" : "http://localhost:5000"
 export default BASE_URL;
 
