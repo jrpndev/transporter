44dd8a6d9ac43b26f79e6f2fe13118d71c51b9b6
Jose Rui Pedraça Neto <josepedraca@bemol.com.br>
2024-11-22 01:46:25 -0300

    feat : changing ip

--- views/project/src/core/api/index.tsx
+++ views/project/src/core/api/index.tsx
@@ -1,4 +1,4 @@
 const environment = process.env.ENVIRONMENT;
-let BASE_URL = environment === 'PRODUCTION' ? "http://54.207.53.119:5000" : "http://localhost:5000"
+let BASE_URL = environment === 'PRODUCTION' ? "http://18.228.203.126:5000" : "http://localhost:5000"
 export default BASE_URL;
 
